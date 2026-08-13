07aa5ab3304d6a8afd081edac62ae86c9dd04cac
Sebastian <sebabutcovich@gmail.com>
2026-08-12 22:35:56 -0300

    ultimo

--- prueba_envio_tension/prueba_envio_tension.ino
+++ prueba_envio_tension/prueba_envio_tension.ino
@@ -103,7 +103,7 @@ void loop() {
 }
 void promedioCorriente() {
   const float vRef = 2.5;            // Tensi�n de offset a 0 A (2.5 V)
-  const float sensibilidad = 0.0267;  // Sensibilidad Canal 1: 0.0267 mV/A -> 0.0267 V/A
+  const float sensibilidad = 0.0267;  // Sensibilidad Canal 1: 267 mV/A -> 0.0267 V/A
   const float alimentacionHall = 5.0; //Tension con el cual se alimenta el sensor.
   float sumaC = 0.0;
   float sumaV = 0.0;
